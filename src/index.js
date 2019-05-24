a825a95e79df8003c22b34b804586be7f589f87b
Juan Villarroel <jluchovs@outlook.com>
2019-05-24 19:27:05 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
+
+// const element = document.createElement('h1')
+// element.innerHTML = 'Hello, Platzi Badge'
+
+
+// container.appendChild(element)
+
+const element = <h1>Hello, Platzi</h1>
+
+const container = document.getElementById('app')
+
+ReactDOM.render(element,container)
